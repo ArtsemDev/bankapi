189cf51911b2c979b64f9ccb1cf16fce3cd2652e
ArtsemDev <thommy.python.dev@gmail.com>
2023-03-13 15:51:11 +0300

    password validator

--- core/schemas/user.py
+++ core/schemas/user.py
@@ -13,9 +13,29 @@ class RegisterForm(BaseModel):
         min_length=2
     )
     password: str = Field(
-        title='User password',
-        description='User password',
+        title='User Password',
+        description='User Password',
         min_length=8,
         max_length=64,
         regex=r'((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})'
     )
+    repeat_password: str = Field(
+        title='Repeat Password',
+        description='Repeat Password',
+        min_length=8,
+        max_length=64,
+        regex=r'((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})'
+    )
+
+    @root_validator(pre=True)
+    def validator(cls, values: dict) -> dict:
+        if values.get('password') != values.get('repeat_password'):
+            raise ValueError('пароли не совпадают')
+
+        if values.get('username').lower() in values.get('password').lower():
+            raise ValueError('имя пользователя не должно содержаться в пароле')
+
+        if values.get('email').lower().split('@')[0] in values.get('password').lower():
+            raise ValueError('почта не должна содержаться в пароле')
+
+        return values
